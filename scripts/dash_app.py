27f311a5c959dcb531ba0a9aab67c1dfd0a03e1d
Killaars <27420806+Killaars@users.noreply.github.com>
2019-12-09 14:23:37 +0100

    functions to project_functions.py

--- scripts/dash_app.py
+++ scripts/dash_app.py
@@ -26,6 +26,7 @@ import datetime
 
 from variables import token
 from variables import p3 as zoom_curve
+from project_functions import get_color,get_linewidth,determine_bbox
 
 # Read shapefile
 path = Path('/home/killaarsl/Documents/CBS3_visualization/') # Main directory
@@ -49,48 +50,6 @@ nr_cameras = pd.DataFrame({'nr_cameras' : [len(df[df['road']==x]['Camera_id'].un
 
 # Get first and last timestamp, in order to get first and last dates and times
 mn, mx = min(df['timestamp']),max(df['timestamp'])
-
-# Kleuren/lijndiktes/teksten afhankelijk van de intensiteit
-def get_color(intensity,max_intensity):
-    ''' returns color value of colormap, depending on the ration between the 
-    intensity and the max intensity of the road. '''
-    cmap = matplotlib.cm.get_cmap('YlOrBr')
-    rgba = cmap(intensity/max_intensity)
-    return 'rgb%s' %(str(rgba[:-1]))
-
-def get_linewidth(intensity,max_intensity):
-    ''' returns the rounded integer of a number between 2 and 10, depending on 
-    the ratio between the intensity and max intensity of the road'''
-    return int(np.round(np.max([4.0,10*(intensity/max_intensity)])))
-
-def determine_bbox(zoom,center_lat,center_lon,fit):
-    ''' returns coordinates of the bounding box of the visible graph. 
-    Scales with the zoom level and should be used to filter the data that 
-    has to be graphed'''
-    from geopy.distance import distance
-    boxdimensions = [800,1200] # size box in pixels [height,width]
-    
-    # determine distance per pixel
-    distanceperpixel = fit(zoom)
-    
-    # distance of the box in meter
-    verbox = boxdimensions[0]*distanceperpixel
-    horbox = boxdimensions[1]*distanceperpixel
-    
-    # Determine the bounding coordinates
-    maxlat = distance(meters=verbox/2).destination(
-        (center_lat, center_lon), 0)[0]
-    minlat = distance(meters=verbox/2).destination(
-        (center_lat, center_lon), 180)[0]
-    maxlon = distance(meters=horbox/2).destination(
-        (center_lat, center_lon), 90)[1]
-    minlon = distance(meters=horbox/2).destination(
-        (center_lat, center_lon), 270)[1]
-    return maxlat,maxlon,minlat,minlon
-
-
-## Wegen afhankelijk van de zoom
-#
 
 #%%
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']                          
